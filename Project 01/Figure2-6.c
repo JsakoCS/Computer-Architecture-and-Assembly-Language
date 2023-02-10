e2cea882c1b7378b579275a7e12bc3dfc6738ff4
JsakoCS <104119408+JsakoCS@users.noreply.github.com>
2023-02-09 17:20:11 -0800

    Figure2-6.c

--- Project 01/Figure2-6.c
+++ Project 01/Figure2-6.c
@@ -1,1 +1,18 @@
+// Programmer : Jsako
 
+#include <stdio.h>
+
+int main()
+{
+    const int BONUS = 10;
+    int exam1;
+    int exam2;
+    int score;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &exam1, &exam2);
+    score = (exam1 + exam2) / 2 + BONUS;
+    printf("Score = %d\n", score);
+
+    return 0;
+}
